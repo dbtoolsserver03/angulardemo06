1c22b72e49665db5d0b07d7f84b0eadda6267ffb
“cuishuangjia” <cuishuangjia@gmail.com>
2021-10-05 08:16:49 +0900

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { NewsComponent } from './components/news/news.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NewsComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
